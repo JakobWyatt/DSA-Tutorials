f1d11b0b13e0cdacccbdad2891ea4d12a3151a54
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-07-29 12:46:36 +0800

    implemented all sorts

--- 1/DSAsorts.py
+++ 1/DSAsorts.py
@@ -8,10 +8,8 @@
 def bubbleSort(A):
     isSorted = False
     while not isSorted:
-        # This algorithm is in place,
-        # restricting our usage of list comprehensions.
         isSorted = True
-        for i in range(0, len(A) - 1):
+        for i in range(len(A) - 1):
             if A[i] > A[i + 1]:
                 isSorted = False
                 A[i], A[i + 1] = A[i + 1], A[i]
@@ -19,11 +17,18 @@ def bubbleSort(A):
 
 
 def insertionSort(A):
-    ...
+    for i in range(len(A)):
+        while i > 0 and A[i] < A[i - 1]:
+            A[i], A[i - 1] = A[i - 1], A[i]
+            i -= 1
+    return A
 
 
 def selectionSort(A):
-    ...
+    for i in range(len(A)):
+        min_idx = min(enumerate(A[i:]), key=lambda x: x[1])[0] + i
+        A[i], A[min_idx] = A[min_idx], A[i]
+    return A
 
 
 def mergeSort(A):
