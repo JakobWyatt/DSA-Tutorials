ede3501a863cceb723539f70153a88119fba4f0c
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-08-05 11:39:09 +0800

    added function declarations for equation solver

---
+++ 2/EquationSolver.py
@@ -0,0 +1,18 @@
+from typing import Any
+from DSAStack import DSAStack
+from DSAQueue import DSAQueue, DSACircularQueue
+
+def solve(equation : str) -> float:
+    pass
+
+def _parseInfixToPostfix(equation : str) -> DSAQueue:
+    pass
+
+def _evaluatePostfix(postfixQueue : DSAQueue) -> float:
+    pass
+
+def _precedenceOf(theOp : str) -> int:
+    pass
+
+def _executeOperation(op : str, op1 : float, op2 : float):
+    pass
