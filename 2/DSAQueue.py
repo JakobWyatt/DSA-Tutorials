d19e2a771a37c634355b1289db315acc97d84742
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-08-05 11:28:21 +0800

    implemented queues

--- 2/DSAQueue.py
+++ 2/DSAQueue.py
@@ -26,3 +26,73 @@ class DSAQueue(ABC):
     @abstractmethod
     def count() -> bool:
         pass
+
+
+class DSAShufflingQueue(DSAQueue):
+    DEFAULT_CAPACITY = 100
+
+    def __init__(self):
+        self._queue = np.empty(DEFAULT_CAPACITY, dtype=object)
+        self._count = 0
+
+    def __init__(self, capacity : int):
+        self._queue = np.empty(capacity, dtype=object)
+        self._count = 0
+
+    def enqueue(self, item : object):
+        self._queue[self._count] = item
+        self._count += 1
+
+    def dequeue(self) -> object:
+        item = self._queue[0]
+        self._count -= 1
+        for i in range(self._count):
+            self._queue[i] = self._queue[i + 1]
+        return item
+
+    def peek(self) -> object:
+        return self._queue[0]
+
+    def isEmpty(self) -> object:
+        return self._count == 0
+
+    def isFull(self) -> object:
+        return self._count == len(self._queue)
+
+    def count(self) -> object:
+        return self._count
+
+
+class DSACircularQueue(DSAQueue):
+    DEFAULT_CAPACITY = 100
+
+    def __init__(self):
+        self._queue = np.empty(DEFAULT_CAPACITY, dtype=object)
+        self._count = 0
+        self._head = 0
+
+    def __init__(self, capacity : int):
+        self._queue = np.empty(capacity, dtype=object)
+        self._count = 0
+        self._head = 0
+
+    def enqueue(self, item : object):
+        self._queue[(self._head + self._count) % len(self._queue)] = item
+        self._count += 1
+
+    def dequeue(self) -> object:
+        self._count -= 1
+        self._head = (self._head + 1) % len(self._queue)
+        return self._queue[self._head - 1]
+
+    def peek(self) -> object:
+        return self._queue[self._head]
+
+    def isEmpty(self) -> object:
+        return self._count == 0
+
+    def isFull(self) -> object:
+        return self._count == len(self._queue)
+
+    def count(self) -> int:
+        return self._count
