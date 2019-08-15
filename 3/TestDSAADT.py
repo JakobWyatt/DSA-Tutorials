5b4aef77ce69fd39ce23b04007577c15eaa0fce9
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-08-15 10:18:23 +0800

    modified queue and stack to use linked lists instead

--- 3/TestDSAADT.py
+++ 3/TestDSAADT.py
@@ -1,50 +1,41 @@
 import unittest
 from typing import Type
-from DSAStack import DSAStack
-from DSAQueue import DSAShufflingQueue, DSACircularQueue, DSAQueue
+from DSAListStack import DSAListStack
+from DSAListQueue import DSAListQueue
 
 class TestDSAADT(unittest.TestCase):
-    def testDSAStack(self):
-        stack = DSAStack(5)
+    def testDSAListStack(self):
+        stack = DSAListStack()
         stack.push(1)
         stack.push(2)
         stack.push(3)
         stack.push(4)
         stack.push(5)
-        self.assertTrue(stack.isFull())
         self.assertEqual(stack.top(), 5)
         self.assertEqual(stack.pop(), 5)
-        self.assertEqual(stack.count(), 4)
         self.assertEqual(stack.pop(), 4)
         stack.push(100)
         self.assertEqual(stack.pop(), 100)
         while not stack.isEmpty():
             stack.pop()
         self.assertTrue(stack.isEmpty())
-    
-    def testDSACircularQueue(self):
-        testDSAQueue(self, DSACircularQueue)
 
-    def testDSAShufflingQueue(self):
-        testDSAQueue(self, DSAShufflingQueue)
-
-def testDSAQueue(testCase : unittest.TestCase, cls : Type[DSAQueue]):
-    queue = cls(5)
-    queue.enqueue(1)
-    queue.enqueue(2)
-    queue.enqueue(3)
-    queue.enqueue(4)
-    queue.enqueue(5)
-    testCase.assertTrue(queue.isFull())
-    testCase.assertEqual(queue.dequeue(), 1)
-    testCase.assertEqual(queue.peek(), 2)
-    testCase.assertEqual(queue.count(), 4)
-    testCase.assertEqual(queue.dequeue(), 2)
-    queue.enqueue(100)
-    while queue.count() > 1:
+    def testDSAListQueue(self):
+        queue = DSAListQueue()
+        queue.enqueue(1)
+        queue.enqueue(2)
+        queue.enqueue(3)
+        queue.enqueue(4)
+        queue.enqueue(5)
+        self.assertEqual(queue.dequeue(), 1)
+        self.assertEqual(queue.peek(), 2)
+        self.assertEqual(queue.dequeue(), 2)
+        queue.enqueue(100)
         queue.dequeue()
-    testCase.assertEqual(queue.dequeue(), 100)
-    testCase.assertTrue(queue.isEmpty())
+        queue.dequeue()
+        queue.dequeue()
+        self.assertEqual(queue.dequeue(), 100)
+        self.assertTrue(queue.isEmpty())
 
 if __name__ == '__main__':
     unittest.main()
