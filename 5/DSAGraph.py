c141ebddbb5198f9059646b8d6b6966c9fa13c38
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-09-02 11:21:36 +0800

    Implemented node class

--- 5/DSAGraph.py
+++ 5/DSAGraph.py
@@ -6,32 +6,41 @@ import linkedLists
 
 class DSAGraphVertex:
     def __init__(self, label: object, value: object):
-        ...
+        self._label = label
+        self._value = value
+        self._adjacent = DSALinkedList()
+        self._visited = False
 
     @property
     def label(self) -> object:
-        ...
+        return self._label
 
     @property
     def value(self) -> object:
-        ...
+        return self._value
 
-    def getAdjacent(self) -> 'DSALinkedList':
-        ...
+    @property
+    def adjacent(self) -> 'DSALinkedList':
+        return self._adjacent
 
     def addEdge(self, vertex: 'DSAGraphVertex') -> None:
-        ...
+        self._adjacent.insertFirst(vertex)
 
     @property
     def visited(self) -> bool:
-        ...
+        return self._visited
 
     @visited.setter
     def visited(self, visited: bool) -> None:
-        ...
+        self._visited = bool(visited)
 
     def __str__(self) -> str:
-        ...
+        return ("{label},{value}:{adj}"
+                .format(label=self.label, value=self.value,
+                        adj=" ".join([x.label for x in self.adjacent])))
+
+    def gv(self) -> str:
+        return "".join([f"{self.label} -- {x.label}\n" for x in self.adjacent])
 
 
 class DSAGraph:
