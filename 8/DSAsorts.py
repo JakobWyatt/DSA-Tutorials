98d86d0afc271d7e585ae4739491e9fbd1916ed5
Jakob Wyatt <jakob.wyatt@gmail.com>
2019-10-07 08:19:30 +0800

    Added already implemented sorts

--- 8/DSAsorts.py
+++ 8/DSAsorts.py
@@ -4,35 +4,56 @@
 # Python file to hold all sorting methods
 #
 
+
 def bubbleSort(A):
-    ... 
+    isSorted = False
+    while not isSorted:
+        isSorted = True
+        for i in range(len(A) - 1):
+            if A[i] > A[i + 1]:
+                isSorted = False
+                A[i], A[i + 1] = A[i + 1], A[i]
+    return A
+
 
 def insertionSort(A):
-    ...
+    for i in range(len(A)):
+        while i > 0 and A[i] < A[i - 1]:
+            A[i], A[i - 1] = A[i - 1], A[i]
+            i -= 1
+    return A
+
 
 def selectionSort(A):
-    ...
+    for i in range(len(A)):
+        min_idx = min(enumerate(A[i:]), key=lambda x: x[1])[0] + i
+        A[i], A[min_idx] = A[min_idx], A[i]
+    return A
+
 
 def mergeSort(A):
     """ mergeSort - front-end for kick-starting the recursive algorithm
     """
     ...
 
+
 def mergeSortRecurse(A, leftIdx, rightIdx):
     ...
 
+
 def merge(A, leftIdx, midIdx, rightIdx):
     ...
+
 
 def quickSort(A):
     """ quickSort - front-end for kick-starting the recursive algorithm
     """
     ...
 
+
 def quickSortRecurse(A, leftIdx, rightIdx):
     ...
 
+
 def doPartitioning(A, leftIdx, rightIdx, pivotIdx):
     ...
-
-
